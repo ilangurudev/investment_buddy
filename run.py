025ce8ea4a04a809a89df0a5325dc5ccc17a1129
ilangurudev <ilangurudev@gmail.com>
2022-02-28 22:39:05 -0500

    enabling run.py

--- run.py
+++ run.py
@@ -12,8 +12,8 @@ bse_downloader = BseDownloader()
 nse_downloader.update_data(prune_weeks=80)
 bse_downloader.update_data(prune_weeks=80)
 
-as_of_date = pendulum.from_format(f"20220228", "YYYYMMDD")  # pendulum.today()
+as_of_date = pendulum.today()  #pendulum.from_format(f"20220228", "YYYYMMDD") 
 data_filter = DataFilters(as_of_date)
 data_filter.apply_all_filters()
 
-# scrape_metrics(data_filter.df_all_filtered, data_filter.date_str)
+scrape_metrics(data_filter.df_all_filtered, data_filter.date_str)
